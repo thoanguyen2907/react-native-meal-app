1692040ef2c92734649be37009994027447badac
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-03-01 12:41:42 +0200

    add navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealsDetailScreen from '../screens/MealsDetailScreen';
-
-
-// const MealsNavigator = createNativeStackNavigator({
-//     Categories: CategoriesScreen,
-//     CategoryMeals: {
-//         screen: CategoryMealsScreen
-//     },
-//     MealDetail: MealsDetailScreen
-// })
+import { HeaderButtons } from 'react-navigation-header-buttons';
+import { Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
 
 // export default NavigationContainer(MealsNavigator)
 const Stack = createNativeStackNavigator();
@@ -21,9 +15,43 @@ const Stack = createNativeStackNavigator();
 const  MealsNavigator = ()   => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={CategoriesScreen} />
-        <Stack.Screen name="Details" component={MealsDetailScreen} />
+      <Stack.Navigator initialRouteName="Category">
+        <Stack.Screen name="Category" component={CategoriesScreen} 
+         options={{
+          title: 'Category',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+        />
+        <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
+        <Stack.Screen name="MealDetails" component={MealsDetailScreen} 
+         options={{
+          title: 'MealDetails',
+          headerStyle: {
+            backgroundColor: '#8A2BE2',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+              title="Favorite"
+              iconName="ios-star"
+              onPress={() => {
+                console.log('Mark as favorite!');
+              }}
+            />
+          </HeaderButtons>
+          )
+        }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+import MealItem from '../components/MealItem';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
-const CategoryMealsScreen = () => {
+
+const CategoryMealsScreen = (props) => {
+
+    const renderItem = itemData => {
+        return (
+            <MealItem
+            title={itemData.item.title}
+            image={itemData.item.imageUrl}
+            duration={itemData.item.duration}
+            complexity={itemData.item.complexity}
+            affordability={itemData.item.affordability}
+            onSelectMeal={() => {
+                props.navigation.navigate('MealDetails',{
+                    mealId : itemData.item.id,
+                    name: 'MealDetails'     
+            })}}
+            >
+
+            </MealItem>
+        )
+    }
+    const catId = props.route.params.categoryId
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
     return (
         <View>
-            <Text>CategoriesScreen</Text>
+            <Text>{selectedCategory.title}</Text>
+            <FlatList data = {displayedMeals} keyExtractor={(item, index) => item.id}
+            renderItem = {renderItem} style = {{width: "100%"}}/>
         </View>
     );
+}
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = props.route.params.categoryId
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    
+    return {
+        headerTitle: selectedCategory.title
+    }
 }
 
 const styles = StyleSheet.create({
